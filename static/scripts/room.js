b8aef0722495a362610568094cc7001f3159beb5
lewisaronmilne <lewisaronmilne@gmail.com>
2015-12-18 13:18:19 +0000

    Shared drawing kinda works
    No settings and still needs to push button to share. But very
    extensible.

--- static/scripts/room.js
+++ static/scripts/room.js
@@ -1,24 +1,36 @@
 var changed = false;
+var canvas = new fabric.Canvas('canvas');
+canvas.isDrawingMode = true;
 
-$('#change').click(function() {
+$('#update').click(function() {
 	$('#label').html($('#field').val());
 	changed = true
+});
+
+$('#modeDrawing').click(function() {
+	if (canvas.isDrawingMode) {
+		canvas.isDrawingMode = false;
+		$('#modeDrawing').html('Selection Mode');
+	}
+	else {
+		canvas.isDrawingMode = true;
+		$('#modeDrawing').html('Drawing Mode');
+	}
 });
 
 function poll() {
 	$.ajax({
 		type: 'POST',
+		dataType: 'json', 
 		url: '/pollRoom',
 		data: pollData(),
-		dataType: 'json', 
 		timeout: 2000,
 		success: function(update) {
-			console.log(update);
-			//codes: -1: error - 0: no changes - 1: changes on server, no changes on client - 2: changes on client, no changes on server - 3 
+			//codes: -1: error - 0: no changes - 1: changes on server - 2: changes on client - 3: changes on server and client
 			switch (update.code) {
 				case -1: case 0: break;
 				case 1: case 3:
-					$('#label').html(update.text);
+					updatePage(update);
 				case 2:
 					vars.version = update.version;
 					break;
@@ -37,18 +49,26 @@ function pollData() {
 		user: 'todo'
 	}
 
-	if (!changed) {
-		data.changed = false;
-	}
-	else {
+	if (changed) {
 		changed = false;
 		data.changed = true;
 		data.text = $('#field').val();
+		data.canvas = JSON.stringify(canvas);
+	}
+	else {
+		data.changed = false;
 	}
 
 	return data;
 }
 
+function updatePage(update) {
+	$('#label').html(update.text);
+	canvas.loadFromJSON(update.canvas);
+	canvas.renderAll();
+}
+
 $(document).ready(function() {
+	updatePage(vars);
 	poll();
 });
