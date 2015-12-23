d0d874500ef717f40397bb589dd63fe6512253b2
Victor <vd3g10@soton.ac.uk>
2015-12-23 12:32:41 +0100

    frontupdate

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ app.config['SECRET_KEY'] = 'vXxGmyT6NxT5MRYD3mKQYx6y'
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return render_template('index.html')
 
 @app.route('/room/<roomId>')
 def room(roomId):
 	room = Rooms.get_by_id(roomId)
 	if (room == None):
-		roomKey = Rooms(id = roomId, version = 0, text = 'hello').put()
+		roomKey = Rooms(id = roomId, version = 0, text = 'hello', canvas = '{"objects":[{"type":"rect","originX":"left","originY":"top","left":100,"top":100,"width":20,"height":20,"fill":"green","stroke":null,"strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeLineJoin":"miter","strokeMiterLimit":10,"scaleX":1,"scaleY":1,"angle":45,"flipX":false,"flipY":false,"opacity":1,"shadow":null,"visible":true,"clipTo":null,"backgroundColor":"","fillRule":"nonzero","globalCompositeOperation":"source-over","rx":0,"ry":0}],"background":""}').put()
 		room = Rooms.get_by_id(roomId)
 
 	vars = room.to_dict()
@@ -23,34 +23,35 @@ def room(roomId):
 
 @app.route('/pollRoom', methods=['POST'])
 def pollRoom():
-	room = Rooms.get_by_id(request.form['id'])
+	sentData = request.form;
+	logging.debug(sentData)
+
+	room = Rooms.get_by_id(sentData['id'])
 	if (room == None):
 		return json.dumps({'code': -1, 'reason': 'eRRoR: No room by that id'})
 
-	update = room.to_dict()
+	hereData = room.to_dict()
 
-	logging.debug(request.form)
-	logging.debug(update)
+	logging.debug(hereData)
 
-	#codes: -1: error - 0: no changes - 1: changes on server, none on client - 2: changes on client, none on server - 3: changes on server and on client
-	if (request.form['changed'] == 'false'):
-		if (int(request.form['version']) == update['version']):
+	#codes: -1: error - 0: no changes - 1: changes on server - 2: changes on client - 3: changes on server and client
+	if (sentData['changed'] == 'false'):
+		if (int(sentData['version']) == hereData['version']):
 			return json.dumps({'code': 0})
 		else:
-			update['code'] = 1
-			return json.dumps(update)
+			hereData['code'] = 1
+			return json.dumps(hereData)
 	else: 
-		inVersion = int(request.form['version'])
-
-		if (inVersion == update['version']):
-			room.version = inVersion + 1
-			room.text = request.form['text']
+		if (int(sentData['version']) == hereData['version']):
+			room.version = int(sentData['version']) + 1
+			room.text = sentData['text']
+			room.canvas =  sentData['canvas']
 			room.put()
-			return json.dumps({'code': 2, 'version': inVersion + 1})
+			return json.dumps({'code': 2, 'version': int(sentData['version']) + 1})
 		else:
 			# todo - diff
-			update['code'] = 3
-			return json.dumps(update)
+			hereData['code'] = 3
+			return json.dumps(hereData)
 
 @app.errorhandler(404)
 def page_not_found(e):
--- models.py
+++ models.py
@@ -4,3 +4,4 @@ import logging, json
 class Rooms(ndb.Model):
 	version = ndb.IntegerProperty(required=True)
 	text = ndb.TextProperty(required=True)
+	canvas = ndb.TextProperty(required=True)
